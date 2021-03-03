7e4e3830d67129090aecab745748fc0877314c88
Sam-R-Taylor <samrtayloradm@gmail.com>
2021-03-03 21:56:59 +0000

    Added test script

---
+++ test_compiler.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+make Makefile
+
+PASSED=0
+FAILED=0
+
+for i in compiler_tests/*; do
+    for j in ${i}/*; do 
+    
+
+    echo "==========================="
+    echo ""
+    echo "Input file : ${j}"
+
+    nj=${j%.c}
+    
+
+    #Skips driver . c files
+    if [[ ${j} == *"driver"* ]]; then
+        continue;
+    fi
+
+
+    ./bin/c_compiler -S ${j} -o test_program.s 
+    mips-linux-gnu-gcc -mfp32 -o test_program.o -c test_program.s
+    mips-linux-gnu-gcc -mfp32 -static -o test_program test_program.o ${nj}_driver.c
+    qemu-mips test_program
+
+
+
+    if [[$? = 0]]
+    then 
+        echo "PASS"
+        PASSED=$(( ${PASSED}+1 ));
+    else 
+        echo "FAIL"
+        FAILED=$(( ${FAILED}+1 ));
+
+
+    fi 
+
+    done 
+done
+
+echo "########################################"
+echo "Passed ${PASSED} , Failed ${FAILED}".
+echo ""
