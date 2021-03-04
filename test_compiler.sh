772442694258038e16efbbf4a342c092c732bba8
Sam-R-Taylor <samrtayloradm@gmail.com>
2021-03-04 09:50:29 +0000

    fixed test_compiler

--- test_compiler.sh
+++ test_compiler.sh
@@ -9,10 +9,6 @@ for i in compiler_tests/*; do
     for j in ${i}/*; do 
     
 
-    echo "==========================="
-    echo ""
-    echo "Input file : ${j}"
-
     nj=${j%.c}
     
 
@@ -21,15 +17,15 @@ for i in compiler_tests/*; do
         continue;
     fi
 
+    echo "==========================="
+    echo "Input file : ${j}"
+
 
     ./bin/c_compiler -S ${j} -o test_program.s 
     mips-linux-gnu-gcc -mfp32 -o test_program.o -c test_program.s
     mips-linux-gnu-gcc -mfp32 -static -o test_program test_program.o ${nj}_driver.c
-    qemu-mips test_program
 
-
-
-    if [[$? = 0]]
+    if qemu-mips test_program;
     then 
         echo "PASS"
         PASSED=$(( ${PASSED}+1 ));
